ee405cd33cf2d35ecc3899909c827831d9aa7d8e
246859 <2633565580@qq.com>
2022-07-25 18:31:38 +0800

    基本功能完成

--- ItemTransfer.js
+++ ItemTransfer.js
@@ -17,14 +17,14 @@ const DEFAULT_LANG = {
             NAME: "§l背包转移",
             IMG: "textures/blocks/beehive_front.png",
             ORDER: 0,
-            MENU:{
-                P2B:{
-                    NAME:"§l背包->箱子",
-                    TIP:"§l是否将背包物品转移至此箱子中"
+            MENU: {
+                P2B: {
+                    NAME: "§l背包->箱子",
+                    TIP: "§l是否将背包物品转移至此箱子中"
                 },
-                B2P:{
-                    NAME:"§l箱子->背包",
-                    TIP:"§l是否将此箱子物品转移到背包中"
+                B2P: {
+                    NAME: "§l箱子->背包",
+                    TIP: "§l是否将此箱子物品转移到背包中"
                 },
 
             }
@@ -32,15 +32,19 @@ const DEFAULT_LANG = {
         BOX_2_BOX: {
             NAME: "§l箱子转移",
             IMG: "textures/blocks/chest_front.png",
-            ORDER: 1
+            ORDER: 1,
+            MENU: {
+                NAME: "§l箱子A->箱子B",
+                TIP: "§l是否将箱子A的物品转移到箱子B中",
+            }
         },
         GIVE_UP: {
             NAME: "§l放弃转移",
             IMG: "textures/ui/book_trash_default.png",
             ORDER: 2
         },
-        CONFIRM:"§l确认",
-        CANCEL:"§l删除"
+        CONFIRM: "§l确认",
+        CANCEL: "§l取消"
     },
     INFO: {
         PACK_2_BOX: {
@@ -48,7 +52,8 @@ const DEFAULT_LANG = {
             START: "潜行点击一个§4要转移的箱子§f,或/tf giveup退出",
         },
         BOX_2_BOX: {
-            NAME: "箱子<-->箱子",
+            NAMEA: "§4箱子A§f-->箱子B",
+            NAMEB: "箱子A-->§a箱子B",
             BOXA: "潜行点击§4待转移的箱子A§f,或/tf giveup退出",
             BOXB: "潜行点击§a要转移的箱子B§f,或/tf giveup退出",
         }
@@ -61,7 +66,13 @@ const DEFAULT_LANG = {
         },
         TRANSFER: {
             REPEAT: "§4当前你已处于其他转移状态，请放弃转移后再选择!",
-            SNEAKING:"§4请在潜行状态下选择!"
+            SNEAKING: "§4请在潜行状态下选择!",
+            REPEATBOX: "§4请勿选择同一个箱子!",
+            CT_ERROR: "§4容器构造异常",
+            POS_ERROR: "§4位置参数解析错误",
+            BLOCK_ERROR: "§4方块解析异常",
+            BL_CT_ERROR: "§4容器解析异常",
+
         }
 
     }
@@ -74,7 +85,6 @@ const DEFAULT_CMD = {
         PERMISSION: PermType.Any,
         FLAG: 0x80,
         ALIAS: "tf"
-
     },
     TRANSFER_ACTION: {
         NAME: "transfer_action",
@@ -107,7 +117,7 @@ const FACTOR = "FACTOR";
 
 //运行时 玩家map
 let plMap = new Map();
-//存储pos对象的map
+//存储Bloc对象的map
 let p2bMap = new Map();
 let b2bMap = new Map();
 
@@ -124,7 +134,7 @@ class Utils {
     }
 
     static debug(data) {
-        mc.broadcast('§4debug:' + data.toString());
+        mc.broadcast('§4debug:' + data);
     }
 
     static tell(pl, msg, type) {
@@ -133,6 +143,23 @@ class Utils {
 
     static isNUll(param) {
         return param === null || param === undefined;
+    }
+
+    static isSamePos(block1, block2) {
+
+        let pos1 = block1.pos;
+        let pos2 = block2.pos;
+
+        // Utils.debug(pos1.x)
+        // Utils.debug(pos2.x)
+        // Utils.debug(pos1.y)
+        // Utils.debug(pos2.y)
+        // Utils.debug(pos1.z)
+        // Utils.debug(pos2.z)
+        // Utils.debug(pos2.dimid)
+        // Utils.debug(pos1.dimid)
+
+        return pos1.x === pos2.x && pos1.y === pos2.y && pos1.z === pos2.z && pos1.dimid === pos2.dimid;
     }
 
     static hasNull(...params) {
@@ -171,7 +198,7 @@ class Utils {
         pl.addTag(FACTOR);
         setTimeout(() => {
             pl.removeTag(FACTOR);
-        }, 500);
+        }, 200);
     }
 
     //检验唯一状态tag
@@ -304,7 +331,9 @@ class CMD {
 
 }
 
-
+/**
+ * 核心数据处理类
+ */
 class Core {
 
     //初始化玩家状态
@@ -367,7 +396,7 @@ class Core {
         //初始化标题状态
         CMD.initTitleStatus(pl);
         //展示标题
-        CMD.titleToPlayer(pl, LANG.INFO.BOX_2_BOX.NAME, LANG.INFO.BOX_2_BOX.BOXA);
+        CMD.titleToPlayer(pl, LANG.INFO.BOX_2_BOX.NAMEA, LANG.INFO.BOX_2_BOX.BOXA);
 
     }
 
@@ -409,30 +438,135 @@ class Core {
         }
     }
 
+    static p2bTriggerProcess(pl, block) {
+        //记录下方块对象
+        p2bMap.set(pl.name, block);
+        pl.sendForm(Form.p2bMenuForm(), Form.p2bMenuFormCallBack);
+    }
+
+    static b2bATriggerProcess(pl, block) {
+        //记录下blockA对象
+        b2bMap.set(pl.name, {boxA: block});
+        //更新title
+        CMD.titleToPlayer(pl, LANG.INFO.BOX_2_BOX.NAMEB, LANG.INFO.BOX_2_BOX.BOXB);
+        //标记进入b2bB的状态
+        pl.removeTag(TAG.B2BA);
+        pl.addTag(TAG.B2BB);
+    }
+
+    static b2bBTriggerProcess(pl, block) {
+        //记录下blockB的对象
+        if (Utils.isSamePos(b2bMap.get(pl.name).boxA, block)) {
+            Utils.tell(pl, LANG.ERROR.TRANSFER.REPEATBOX);
+            return;
+        }
+
+        b2bMap.get(pl.name).boxB = block;
+        pl.sendModalForm(
+            LANG.FORM.BOX_2_BOX.MENU.NAME,
+            LANG.FORM.BOX_2_BOX.MENU.TIP,
+            LANG.FORM.CONFIRM,
+            LANG.FORM.CANCEL,
+            (player, result) => {
+                //TODO 这里点击确认则进行物品转移
+                if (result) {
+                    //创建转移处理器
+                    let b2bProcessor = new Transfer(
+                        Transfer.transformBlockToCt(pl, b2bMap.get(pl.name).boxA),
+                        Transfer.transformBlockToCt(pl, b2bMap.get(pl.name).boxB));
+                    b2bProcessor.processor(pl);
+                    Core.cleanPlayerStatus(pl);
+                    Core.b2baCallBack(pl);
+                }
+            }
+        );
+    }
+
     static triggerCallback(pl, item, block, side, pos) {
 
         //参数校验
         if (Utils.hasNull(pl, item, block, side, pos) || pl.hasTag(FACTOR)) return;
 
-        //潜行判断
-        if (!pl.sneaking){
-            Utils.tell(pl,LANG.ERROR.TRANSFER.SNEAKING);
-            return;
-        }
+        //潜行状态
+        if (!pl.sneaking) return;
 
         //win10多次触发解决
         Utils.resolveWin10Problem(pl);
 
         if (Utils.isOnlyStatusTag(pl, TAG.P2B)) {
-            p2bMap.set(pl.name,pos);
-            pl.sendForm(Form.p2bMenuForm(),Form.p2bMenuFormCallBack);
+            Core.p2bTriggerProcess(pl, block);
         } else if (Utils.isOnlyStatusTag(pl, TAG.B2BA)) {
-            Utils.debug(TAG.B2BA)
+            Core.b2bATriggerProcess(pl, block);
         } else if (Utils.isOnlyStatusTag(pl, TAG.B2BB)) {
-            Utils.debug(TAG.B2BB)
+            Core.b2bBTriggerProcess(pl, block);
         }
 
     }
+}
+
+class Transfer {
+
+    constructor(ct1, ct2) {
+
+        if (Utils.hasNull(ct1, ct2)) {
+            Utils.tell(LANG.ERROR.TRANSFER.CT_ERROR);
+            return;
+        }
+
+        this.containerA = ct1;
+        this.containerB = ct2;
+
+    }
+
+    //将坐标对象转换成容器对象
+    static transformBlockToCt(pl, block) {
+
+        if (Utils.hasNull(pl, block)) {
+            Utils.tell(LANG.ERROR.TRANSFER.BLOCK_ERROR);
+            return;
+        }
+
+        let ct = block.getContainer();
+
+        if (Utils.hasNull(ct)) {
+            Utils.tell(LANG.ERROR.TRANSFER.BL_CT_ERROR);
+        }
+
+        return ct;
+    }
+
+    processor(pl){
+        Utils.debug(this.containerA.size);
+        Utils.debug(this.containerB.size);
+
+        let ct1 = this.containerA;
+        let ct2 = this.containerB;
+
+        for (let i = 0; i < ct1.size; i++){
+            //获取当前方格的物品
+           let item = ct1.getItem(i);
+
+           //非空判断
+           if (item.isNull()) continue;
+
+           //获取nbt
+           let nbt = item.getNbt();
+
+           //根据nbt生成新物品
+           let newItem = mc.newItem(nbt);
+
+           //删除原物品
+           if (!item.setNull()) break;
+
+           //刷新玩家容器
+           if (!pl.refreshItems()) break;
+
+           //放入另一个容器中
+           if (!ct2.addItem(newItem)) break;
+
+        }
+    }
+
 }
 
 /**
@@ -472,7 +606,7 @@ class Form {
         }
     }
 
-    static p2bMenuForm(){
+    static p2bMenuForm() {
         let fm = mc.newSimpleForm();
         fm.setTitle(LANG.FORM.PACK_2_BOX.NAME);
         fm.addButton(LANG.FORM.PACK_2_BOX.MENU.P2B.NAME);
@@ -480,8 +614,8 @@ class Form {
         return fm;
     }
 
-    static p2bMenuFormCallBack(pl,id){
-        if (Utils.hasNull(pl,id)) return;
+    static p2bMenuFormCallBack(pl, id) {
+        if (Utils.hasNull(pl, id)) return;
 
         switch (id) {
             //p2b
@@ -491,8 +625,17 @@ class Form {
                     LANG.FORM.PACK_2_BOX.MENU.P2B.TIP,
                     LANG.FORM.CONFIRM,
                     LANG.FORM.CANCEL,
-                    ()=>{});
-            }break;
+                    (player, result) => {
+                        //TODO 点击确认则进行物品转移
+                        if (result) {
+                            let p2bProcessor = new Transfer(
+                                pl.getInventory(),
+                                Transfer.transformBlockToCt(pl, p2bMap.get(pl.name)));
+                            p2bProcessor.processor(pl);
+                        }
+                    });
+            }
+                break;
             //b2p
             case 1 : {
                 pl.sendModalForm(
@@ -500,7 +643,15 @@ class Form {
                     LANG.FORM.PACK_2_BOX.MENU.B2P.TIP,
                     LANG.FORM.CONFIRM,
                     LANG.FORM.CANCEL,
-                    ()=>{});
+                    (player, result) => {
+                        //TODO 点击确认则进行物品转移
+                        if (result) {
+                            let b2pProcessor = new Transfer(
+                                Transfer.transformBlockToCt(pl, p2bMap.get(pl.name)),
+                                pl.getInventory());
+                            b2pProcessor.processor(pl);
+                        }
+                    });
             }
         }
     }
